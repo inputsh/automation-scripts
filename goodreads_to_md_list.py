d08be2308e1a586a622db45443f54c498c16e25f
Aleksandar Todorović <aleksandar@r3bl.me>
2017-08-29 23:50:41 +0200

    goodreads_to_md_list.py now calculates reading goals

--- goodreads_to_md_list.py
+++ goodreads_to_md_list.py
@@ -13,7 +13,7 @@ parameters = {
     "sort" : "date_read",
     "order" : "a",
     "per_page" : 200,
-    "key" : "<API_KEY_GOES_HERE>"
+    "key" : "jtHlppFX53E4ybfdovbA"
 }
 
 # make a GET request
@@ -43,7 +43,26 @@ root = ET.fromstring(r.text)
 # </GoodreadsResponse>
 f = open('00_books.md', 'w')
 
-f.write("# Books\n\n> After all any book is just a list of thoughts of another guy, who might be wrong.\n\nThis file is the exported version of my [Goodreads `read` shelf](https://www.goodreads.com/review/list/37688006-aleksandar-todorovi?shelf=read), synced using my [`goodreads_to_md_list.py`](https://github.com/aleksandar-todorovic/automation-scripts#goodreads_to_md_listpy) script. Having a Git backup prevents me from feeling that I might be vendor-locked-in, even though it's just a freaking list of books that I could probably re-create.\n\nThe books below are in order of me marking them as read on Goodreads, from oldest to newest. The stars are my subjective opinion of the book on the scale from one to five.\n\nFormat: ``* `author_name` book_title (rating) [[isbn13](link_to_goodreads)]``\n\n## Books I have read\n\n")
+f.write("# Books\n\n> After all any book is just a list of thoughts of another guy, who might be wrong.\n\nThis file is the exported version of my [Goodreads `read` shelf](https://www.goodreads.com/review/list/37688006-aleksandar-todorovi?shelf=read), synced using my [`goodreads_to_md_list.py`](https://github.com/aleksandar-todorovic/automation-scripts#goodreads_to_md_listpy) script. Having a Git backup prevents me from feeling that I might be vendor-locked-in, even though it's just a freaking list of books that I could probably re-create.\n\nThe books below are in order of me marking them as read on Goodreads, from oldest to newest. The stars are my subjective opinion of the book on the scale from one to five.\n\nFormat: ``* `author_name` book_title (rating) [[isbn13](link_to_goodreads)]``\n\n")
+
+# Set reading goals to zero
+goal_2015 = 0
+goal_2016 = 0
+goal_2017 = 0
+
+for reviews in root.find('reviews'):
+    read_at = reviews.find('read_at').text
+    # Final four characters represent a year
+    year = read_at[-4:]
+    if int(year) == 2015:
+        goal_2015 = goal_2015 + 1
+    elif int(year) == 2016:
+        goal_2016 = goal_2016 + 1
+    elif int(year) == 2017:
+        goal_2017 = goal_2017 + 1
+
+# Print down the reading goals
+f.write("## Reading Goals:\n\n* 2015: " + str(goal_2015) + "/25\n* 2016: " + str(goal_2016) + "/20\n* 2017: " + str(goal_2017) + "/25\n\n## Books I have read\n\n")
 
 for review in root.find('reviews'):
     book = review.find('book')
