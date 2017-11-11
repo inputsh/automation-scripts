3da16c1ee2fbf8be06b5997998d10176b980aacc
Francisco <franciscouzo@gmail.com>
2017-11-11 16:00:54 -0300

    Rewrite hn500-to-simplepush.py

--- hn500-to-simplepush.py
+++ hn500-to-simplepush.py
@@ -1,38 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+from multiprocessing import Pool
 import requests
-import json
-import sys, os
 
-#TODO: Speed this script up. It currently takes ~28 seconds to finish.
-#TODO: Add Wallabag integration as bonus points.
+# TODO: Add Wallabag integration as bonus points.
 
-# Fetches the IDs of top HN stories at the moment.
-r = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+SIMPLEPUSH_ID = '{{ SIX CHARACTER SIMPLEPUSH ID HERE }}'
+BASE_URL = 'https://hacker-news.firebaseio.com/v0'
 
-simplepush_id = "{{ SIX CHARACTER SIMPLEPUSH ID HERE}}"
-hn_id = ""
-i = 0
-score = 0
 
-# Fetches the details from the first 30 story IDs (since the HN homepage contains 30 stories).
-while i < 30:
-    hn_id = json.loads(r.text)[i]
-    r2 = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(hn_id) + '.json?print=pretty')
-    # Gets the score of the ID
-    score = json.loads(r2.text)['score']
-    # Checks if the score is bigger than 500 and proceeds if it is.
-    if score > 500:
-        print ("\nHN ID: " + str(hn_id))
-        # Open the history file
-        with open(".hn-to-simplepush.history", "r+") as history_file:
-            # Checks if the ID is already in the notification history.
-            line_found = any(str(hn_id) in line for line in history_file)
-            print("line_found = " + str(line_found))
-            if not line_found:
-                # Append the ID to a file so that this ID wouldn't trigger notifications over and over again every time you run the script.
-                history_file.write(str(hn_id) + "\n")
-                # Send a Simplepush notification
-                cmd = "curl --data 'key=" + simplepush_id + "&title=HN500: " + json.loads(r2.text)['title'] + "'" + " --data-urlencode 'msg=" + json.loads(r2.text)['url'] + "' https://api.simplepush.io/send"
-                os.system(cmd)
-    i = i + 1
+def main():
+    with open('.hn-to-simplepush.history', 'a+') as history_file:
+        history_file.seek(0)
+        seen_ids = {int(line) for line in history_file}
+
+        topstories_ids = requests.get('{}/topstories.json'.format(BASE_URL)).json()
+
+        with Pool(8) as pool:
+            urls = []
+            for topstory_id in topstories_ids[:30]:
+                if topstory_id not in seen_ids:
+                    urls.append('{}/item/{}.json'.format(BASE_URL, topstory_id))
+
+            for topstory_id, topstory_r in zip(topstories_ids, pool.imap(requests.get, urls)):
+                topstory = topstory_r.json()
+
+                if topstory['score'] > 500:
+                    print('HN ID:', topstory_id)
+
+                    requests.post('https://api.simplepush.io/send', data={
+                        'key': SIMPLEPUSH_ID,
+                        'title': 'HN500: ' + topstory['title'],
+                        'msg': topstory['url']
+                    })
+
+                    history_file.write('{}\n'.format(topstory_id))
+
+
+if __name__ == '__main__':
+    main()
